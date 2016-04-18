f9034ad21b4146a79e57dc431bcf4b67eedac9be
email <zephryin@gmail.com>
2016-04-18 12:10:11 -0700

    typo

--- src/twitter_followers_list.py
+++ src/twitter_followers_list.py
@@ -38,7 +38,6 @@ class get_twitter_followers(object):
             x = ' '.join([str(x) for x in ls['TwitterFollowers']])
             self.strings.append(x)
             self.titles.append(ls['Title'])
-        return list_of_strings, list_of_titles
 
     def list_followers(self):
         self.list_of_followers = []
@@ -58,9 +57,8 @@ class get_twitter_followers(object):
     def get_overlapping_users(self, Num=10):
         if self.list_of_followers is None:
             self.list_followers()
-        strings,titles = self.create_sparse_matrix(self.list_of_followers)
-        self.titles = titles
-        final = CV.fit_transform(strings)
+        self.create_sparse_matrix(self.list_of_followers)
+        final = CV.fit_transform(self.strings)
         rowsum_m = csr_matrix.sum(final, axis=0)
         self.totalTwitters = final.shape[1]
         rowsum_a = np.array(rowsum_m).reshape(rowsum_m.shape[1],)
@@ -83,7 +81,7 @@ class get_twitter_followers(object):
         if mat.shape[1] > max_num:
             max_num = mat.shape[1]
         df = pd.DataFrame(mat[:, 0:max_num])
-        df.index = titles
+        df.index = self.titles
         a = df.sum(axis=1)
         mask2 = a > 0
         return df.loc[mask2, :]
