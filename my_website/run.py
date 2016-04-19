f70fad61fd92283acf6fb23a083072f2103f01b6
email <zephryin@gmail.com>
2016-04-19 14:51:02 -0700

    trying to place three photos in the landing page

--- my_website/run.py
+++ my_website/run.py
@@ -6,12 +6,17 @@ Created on Tue Apr 19 10:39:34 2016
 """
 
 from flask import Flask, render_template
+import pandas as pd
+
 app = Flask(__name__)
+
+df = pd.read_csv("../data/Podcast_additional_info.csv", low_memory=False)
+Titles = df.Title.values[0:200]
 
 # home page
 @app.route('/')
 def index():
-    return render_template('index.html', title='Hello!')
+    return render_template('index.html', title='Hello!', seq=Titles)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
