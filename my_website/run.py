e9ac6abeff1f36d3b1cf597e6a73caab72452117
email <zephryin@gmail.com>
2016-04-26 14:13:33 -0700

    made both python2 compatible

--- my_website/run.py
+++ my_website/run.py
@@ -7,11 +7,26 @@ Created on Tue Apr 19 10:39:34 2016
 
 from flask import Flask, render_template, request
 import pandas as pd
+import unicodedata
 
 app = Flask(__name__)
 
-df = pd.read_csv("../data/Podcast_additional_info.csv", low_memory=False)
+
+def whatisthis(s):
+    try:
+        s.decode('ascii')
+        return s
+    except:
+        return s.decode("utf-8")
+
+
+
+df = pd.read_pickle("../data/FinalPodcastMeta.pkl")
 df = df.iloc[0:200, :]
+df.Title = df.Title.apply(lambda x: unicode(x.decode('utf-8')))
+df.Title= df['Title'].str.lower()
+df.iTunesSubtitle = df.iTunesSubtitle.apply(lambda x: unicode(x.decode('utf-8')))
+
 
 
 def stars(x):
@@ -23,11 +38,12 @@ def stars(x):
     
 def get_meta(temp2):
     ls_info = []
+    temp2['Title'] = temp2['Title'].str.title()
     keep = ['AverageRating','Link','iTunesSubtitle', 'Title']
     for i in temp2.index:
         ls_info.append(temp2[keep].iloc[i].to_dict())
     return ls_info
-    
+
 
 # home page
 @app.route('/')
@@ -38,8 +54,8 @@ def index():
 @app.route('/recommender', methods=['GET','POST'])
 def recommender():
     select = request.form.get('Podcast')
-    pod = str(select)
-    temp = df[df.Title==pod]
+    pod = unicode(select)
+    temp = df[df.Title==pod.lower()]
     pred = df.Title[0:10].values
     temp2 = df[df.Title.isin(pred)]
     prediction_info = get_meta(temp2)
@@ -51,9 +67,3 @@ def recommender():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
-
-
-#def word_counter():
-#    text = [str(request.form['user_input'])]
-#    page = 'The predicted section name is: '
-#    return page + predict_section_name(text)
