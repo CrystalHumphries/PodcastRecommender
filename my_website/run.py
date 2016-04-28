9f3bd995ba44ce5e69d4c9913e75408750803304
Ubuntu <ubuntu@ip-172-31-44-169.us-west-2.compute.internal>
2016-04-28 03:11:58 +0000

    new changes to recommendations.html

--- my_website/run.py
+++ my_website/run.py
@@ -8,25 +8,17 @@ Created on Tue Apr 19 10:39:34 2016
 from flask import Flask, render_template, request
 import pandas as pd
 import unicodedata
+import graphlab as gl
+import numpy as np
 
 app = Flask(__name__)
 
-
-def whatisthis(s):
-    try:
-        s.decode('ascii')
-        return s
-    except:
-        return s.decode("utf-8")
-
-
-
-df = pd.read_pickle("../data/FinalPodcastMeta.pkl")
-df = df.iloc[0:200, :]
+df = pd.read_pickle("../data/CheckFirst_NewMeta.pkl")
+df.reset_index(inplace=True)
 df.Title = df.Title.apply(lambda x: unicode(x.decode('utf-8')))
 df.Title= df['Title'].str.lower()
 df.iTunesSubtitle = df.iTunesSubtitle.apply(lambda x: unicode(x.decode('utf-8')))
-
+d = gl.load_model("../src/item_model")
 
 
 def stars(x):
@@ -41,24 +33,25 @@ def get_meta(temp2):
     temp2['Title'] = temp2['Title'].str.title()
     keep = ['AverageRating','Link','iTunesSubtitle', 'Title']
     for i in temp2.index:
-        ls_info.append(temp2[keep].iloc[i].to_dict())
+        ls_info.append(temp2[keep].loc[i].to_dict())
     return ls_info
-
 
 # home page
 @app.route('/')
 def index():
-    return render_template('index.html', title='Hello!', seq=df.Title.values)
-
+    return render_template('index.html', title='New Podcasts', seq=df.Title.values)
 
 @app.route('/recommender', methods=['GET','POST'])
 def recommender():
     select = request.form.get('Podcast')
     pod = unicode(select)
     temp = df[df.Title==pod.lower()]
-    pred = df.Title[0:10].values
+    pred = d.get_similar_items(gl.SArray([pod]))
+    pred = pd.Series(pred['similar'])
+    pred = pred.str.lower()
     temp2 = df[df.Title.isin(pred)]
     prediction_info = get_meta(temp2)
+
     return render_template('Recommendations.html', title='Hello!', 
                            podcast=pod, summary=temp.iTunesSummary.values[0],
                            ratings = temp.RatingCount.values[0],
@@ -67,4 +60,3 @@ def recommender():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
-
